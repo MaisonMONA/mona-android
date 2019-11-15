baa6abed2a4f288683ab8383d5e9c7c19af5d9ea
Nem M <test@test.com>
2019-11-15 15:03:32 -0500

    Minor changes

--- app/src/main/java/com/example/dajc/tabs/MainActivity.java
+++ app/src/main/java/com/example/dajc/tabs/MainActivity.java
@@ -212,14 +212,4 @@ public class MainActivity extends AppCompatActivity{
         return super.dispatchTouchEvent( event );
     }
 
-    public void reload() {
-        Intent intent = getIntent();
-        overridePendingTransition(0, 0);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-        finish();
-        overridePendingTransition(0, 0);
-        startActivity(intent);
-    }
-
-
 }
